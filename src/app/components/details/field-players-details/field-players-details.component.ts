38d981c3bf59e38e9e1fb2a30983e69124cfb195
DESKTOP-PCILHHT\Gerard <gerard.fernandez@col.vueling.com>
2022-07-24 20:23:37 +0200

    no message

--- src/app/components/details/field-players-details/field-players-details.component.ts
+++ src/app/components/details/field-players-details/field-players-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PlayersService } from 'src/app/services/players.service';
 import { ActivatedRoute } from '@angular/router';
 import { NavigationService } from 'src/app/services/navigation.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-field-players-details',
@@ -13,12 +14,16 @@ export class FieldPlayersDetailsComponent implements OnInit {
   id: any;
   fieldPlayer: any;
 
-  constructor(private playersService: PlayersService, private activatedRoute: ActivatedRoute, public navigation:NavigationService) {
+  constructor(private playersService: PlayersService, private activatedRoute: ActivatedRoute, public navigation:NavigationService, private translate: TranslateService) {
     this.id = this.activatedRoute.snapshot.paramMap.get("id");
 
     this.fieldPlayer = this.playersService.getPlayerById(this.id);
 
     this.navigation.startSaveHistory();
+  }
+
+  getLang() {
+    return this.translate.currentLang;
   }
 
   ngOnInit(): void {
--- src/app/components/details/goalkeepers-details/goalkeepers-details.component.ts
+++ src/app/components/details/goalkeepers-details/goalkeepers-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PlayersService } from 'src/app/services/players.service';
 import { ActivatedRoute } from '@angular/router';
 import { NavigationService } from 'src/app/services/navigation.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-goalkeepers-details',
@@ -13,12 +14,16 @@ export class GoalkeepersDetailsComponent implements OnInit {
   id: any;
   goalkeeper: any;
 
-  constructor(private playersService: PlayersService, private activatedRoute: ActivatedRoute, public navigation:NavigationService) {
+  constructor(private playersService: PlayersService, private activatedRoute: ActivatedRoute, public navigation:NavigationService, private translate: TranslateService) {
     this.id = this.activatedRoute.snapshot.paramMap.get("id");
 
     this.goalkeeper = this.playersService.getPlayerById(this.id);
 
     this.navigation.startSaveHistory();
+  }
+
+  getLang() {
+    return this.translate.currentLang;
   }
 
   ngOnInit(): void {
